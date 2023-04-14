6d21197b709eb3c819b3b48988ceb2007ff7358f
L3fty69 <83811912+L3fty69@users.noreply.github.com>
2023-04-14 11:03:28 +0200

    Drag system done

--- Assets/dragAll.cs
+++ Assets/dragAll.cs
@@ -2,16 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dragAll : MonoBehaviour{
+public class DragAll : MonoBehaviour
+{
     private Transform dragging = null;
     private Vector3 offset;
+    [SerializeField] private LayerMask movableLayers;
 
-
+    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            // Cast our own ray.
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero,
+                                                 float.PositiveInfinity, movableLayers);
+            if (hit)
+            {
+                // If we hit, record the transform of the object we hit.
+                dragging = hit.transform;
+                // And record the offset.
+                offset = dragging.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // Stop dragging.
+            dragging = null;
+        }
+
+        if (dragging != null)
+        {
+            // Move object, taking into account original offset.
+            dragging.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
         }
     }
-
+} 
