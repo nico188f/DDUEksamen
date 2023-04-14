0784f42f630a192b971748cee9e8aa49f32cb6c7
L3fty69 <83811912+L3fty69@users.noreply.github.com>
2023-04-14 09:31:08 +0200

    start på dragscript

---
+++ Assets/dragAll.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragAll : MonoBehaviour{
+    private Transform dragging = null;
+    private Vector3 offset;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        }
+    }
+
