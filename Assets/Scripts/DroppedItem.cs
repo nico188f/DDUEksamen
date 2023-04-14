6cd42b361bbdbc7b7a3d05de4071f7aa6919ebd1
nico188f <35289468+nico188f@users.noreply.github.com>
2023-04-14 09:33:01 +0200

    Items will not follow if inventory is full

--- Assets/Scripts/DroppedItem.cs
+++ Assets/Scripts/DroppedItem.cs
@@ -6,13 +6,4 @@ using UnityEngine;
 public class DroppedItem : MonoBehaviour
 {
     public Item item;
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("AttractionZone"))
-        {
-            gameObject.layer = 6;
-
-        }
-    }
 }
