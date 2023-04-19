ae4c60d2e26459ca584a973a585df212d7311faf
nico188f <35289468+nico188f@users.noreply.github.com>
2023-04-19 11:20:53 +0200

    Blake Spritesheet

--- Assets/Scripts/ClassesAndScriptebleObjects/Conversation.cs
+++ Assets/Scripts/ClassesAndScriptebleObjects/Conversation.cs
@@ -8,44 +8,6 @@ using UnityEngine.Events;
 [CreateAssetMenu(fileName = "New Conversation", menuName = "Scriptable Object/Create New Conversation")]
 public class Conversation : ScriptableObject
 {
-    [Serializable]
-    public class ItemAndAmount
-    {
-        public Item item;
-        public int amount;
-    }
-    [Serializable]
-    public class Responce
-    {
-        public string responceMessage;
-
-        [Header("Requirement for responce")]
-        public ItemAndAmount RequiredItems;
-        public int RequiredAmountOfMoney;
-
-        [Header("Next step")]
-        public ConversationPart NPCResponce;
-        [Tooltip("If you are in a nested conversation you can navigate out by using this list.")]
-        public List<int> NPCResponceIndex = new List<int>();
-    }
-    [Serializable]
-    public class ConversationPart
-    {
-        public string NPCDialog;
-        public string carecterName;
-
-        [Header("Reward")]
-        public List<ItemAndAmount> Rewards = new List<ItemAndAmount>();
-        public int cashReward;
-
-        [Header("Next Step")]
-        public UnityEvent OnConversation;
-
-        public List<Responce> Responces = new List<Responce>();
-        [Tooltip("If you are in a nested conversation you can navigate out by using this list.")]
-        public List<int> NextConversationPartIndex = new List<int>();
-    }
-    [SerializeField]
-    public List<ConversationPart> ConversationParts = new List<ConversationPart>();
+    public List<DialogMessage> ConversationParts = new List<DialogMessage>();
 
 }
