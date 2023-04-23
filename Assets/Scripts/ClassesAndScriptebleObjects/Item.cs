6de7d90b95613d786359dc9e25308de7b47fbd05
nico188f <nico188f@gmail.com>
2023-04-24 00:08:48 +0200

    s

--- Assets/Scripts/ClassesAndScriptebleObjects/Item.cs
+++ Assets/Scripts/ClassesAndScriptebleObjects/Item.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//kilde: https://medium.com/@yonem9/create-an-unity-inventory-part-1-basic-data-model-3b54451e25ec
+
 [CreateAssetMenu(fileName = "New Item", menuName = "Scriptable Object/Create New Item")]
 public class Item : ScriptableObject
 {
